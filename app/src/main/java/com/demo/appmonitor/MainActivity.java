6d9fcdf5a377161cc285df26f0f3d6ad20707f90
LinearLaw <17817363770@163.com>
2020-11-10 17:08:23 +0800

    新增view pager

--- app/src/main/java/com/demo/appmonitor/MainActivity.java
+++ app/src/main/java/com/demo/appmonitor/MainActivity.java
@@ -3,10 +3,20 @@ package com.demo.appmonitor;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.dev.sacot41.scviewpager.DotsView;
+import com.dev.sacot41.scviewpager.SCPositionAnimation;
+import com.dev.sacot41.scviewpager.SCViewAnimation;
+import com.dev.sacot41.scviewpager.SCViewAnimationUtil;
+import com.dev.sacot41.scviewpager.SCViewPager;
+import com.dev.sacot41.scviewpager.SCViewPagerAdapter;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -16,6 +26,7 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import java.util.Calendar;
 import java.util.List;
@@ -27,25 +38,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_applist, R.id.nav_research)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
 
-        // 判断是否有权限，无权限则跳到权限设置界面
-//        if (!isPermission()) {
-//            startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
-//        }
+        // 加载启动页 view pager
+        renderViewPager();
+
+        // 加载toolbar - 也就是以前的那坨
+        renderToolBar();
     }
 
     @Override
@@ -77,6 +75,158 @@ public class MainActivity extends AppCompatActivity {
         }else {
             return true;
         }
+    }
+
+    // 渲染tool bar - 就是以前的那坨
+    public void renderToolBar(){
+        // 在renderViewPager里面已经写了这一句
+        // setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_applist, R.id.nav_research)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+    // ------------- 下面这坨都是view pager -------------
+
+    // ViewPager 参数
+    private static final int NUM_PAGES = 4;
+    private SCViewPager mViewPager;
+    private SCViewPagerAdapter mPageAdapter;
+    private DotsView mDotsView;
+
+    // 启动应用时，渲染viewPager
+    public void renderViewPager(){
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        mViewPager = (SCViewPager) findViewById(R.id.viewpager_main_activity);
+        mDotsView = (DotsView) findViewById(R.id.dotsview_main);
+        mDotsView.setDotRessource(R.drawable.dot_selected, R.drawable.dot_unselected);
+        mDotsView.setNumberOfPage(NUM_PAGES);
+
+        mPageAdapter = new SCViewPagerAdapter(getSupportFragmentManager());
+        mPageAdapter.setNumberOfPage(NUM_PAGES);
+
+        // 设置背景颜色
+        mPageAdapter.setFragmentBackgroundColor(R.color.view_pager_green);
+
+        mViewPager.setAdapter(mPageAdapter);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                mDotsView.selectDot(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) { }
+        });
+
+        final Point size = SCViewAnimationUtil.getDisplaySize(this);
+
+        // ------------- 下面这一坨都是操作元素展示动画，动画效果的参数就在这里改 -------------
+        // 1、第一页，第一个元素，在滚动时，移动到上方的y/2处
+        View nameTag = findViewById(R.id.imageview_main_activity_name_tag);
+        SCViewAnimation nameTagAnimation = new SCViewAnimation(nameTag);
+        nameTagAnimation.addPageAnimation(new SCPositionAnimation(this, 0,0,-size.y/2));
+        mViewPager.addAnimation(nameTagAnimation);
+
+        // 2、第一页，第二个元素，滚动时，移动到x轴的左侧x/2处
+        View currentlyWork = findViewById(R.id.imageview_main_activity_currently_work);
+        SCViewAnimation currentlyWorkAnimation = new SCViewAnimation(currentlyWork);
+        currentlyWorkAnimation.addPageAnimation(new SCPositionAnimation(this, 0, -size.x, 0));
+        mViewPager.addAnimation(currentlyWorkAnimation);
+
+        // 4、第二页，第一个元素，从第一页离开，从第二页离开的动画
+        View mobileView = findViewById(R.id.imageview_main_activity_mobile);
+        SCViewAnimation mobileAnimation = new SCViewAnimation(mobileView);
+        mobileAnimation.startToPosition((int)(size.x*1.5), null);
+        mobileAnimation.addPageAnimation(new SCPositionAnimation(this, 0, -(int)(size.x*1.5), 0));
+        mobileAnimation.addPageAnimation(new SCPositionAnimation(this, 1, -(int)(size.x*1.5), 0));
+        mViewPager.addAnimation(mobileAnimation);
+
+        // 5、第二页，第二个元素，也是设置第一页和第二页离开的动画
+        View djangoView = findViewById(R.id.imageview_main_activity_django_python);
+        SCViewAnimation djangoAnimation = new SCViewAnimation(djangoView);
+        djangoAnimation.startToPosition(null, -size.y);
+        djangoAnimation.addPageAnimation(new SCPositionAnimation(this, 0, 0, size.y));
+        djangoAnimation.addPageAnimation(new SCPositionAnimation(this, 1, 0, size.y));
+        mViewPager.addAnimation(djangoAnimation);
+
+        View commonlyView = findViewById(R.id.imageview_main_activity_commonly);
+        SCViewAnimation commonlyAnimation = new SCViewAnimation(commonlyView);
+        commonlyAnimation.startToPosition(size.x, null);
+        commonlyAnimation.addPageAnimation(new SCPositionAnimation(this, 0, -size.x, 0));
+        commonlyAnimation.addPageAnimation(new SCPositionAnimation(this, 1, -size.x, 0));
+        mViewPager.addAnimation(commonlyAnimation);
+
+        View butView = findViewById(R.id.imageview_main_activity_but);
+        SCViewAnimation butAnimation = new SCViewAnimation(butView);
+        butAnimation.startToPosition(size.x, null);
+        butAnimation.addPageAnimation(new SCPositionAnimation(this, 1, -size.x,0));
+        butAnimation.addPageAnimation(new SCPositionAnimation(this, 2, -size.x,0));
+        mViewPager.addAnimation(butAnimation);
+
+        View diplomeView = findViewById(R.id.imageview_main_activity_diploma);
+        SCViewAnimation diplomeAnimation = new SCViewAnimation(diplomeView);
+        diplomeAnimation.startToPosition((size.x *2), null);
+        diplomeAnimation.addPageAnimation(new SCPositionAnimation(this, 1, -size.x*2,0));
+        diplomeAnimation.addPageAnimation(new SCPositionAnimation(this, 2, -size.x*2 ,0));
+        mViewPager.addAnimation(diplomeAnimation);
+
+        View whyView = findViewById(R.id.imageview_main_activity_why);
+        SCViewAnimation whyAnimation = new SCViewAnimation(whyView);
+        whyAnimation.startToPosition(size.x, null);
+        whyAnimation.addPageAnimation(new SCPositionAnimation(this, 1, -size.x, 0));
+        whyAnimation.addPageAnimation(new SCPositionAnimation(this, 2, -size.x, 0));
+        mViewPager.addAnimation(whyAnimation);
+
+        View futureView = findViewById(R.id.imageview_main_future);
+        SCViewAnimation futureAnimation = new SCViewAnimation(futureView);
+        futureAnimation.startToPosition(null, -size.y);
+        futureAnimation.addPageAnimation(new SCPositionAnimation(this, 2, 0, size.y));
+        futureAnimation.addPageAnimation(new SCPositionAnimation(this, 3, -size.x, 0));
+        mViewPager.addAnimation(futureAnimation);
+
+        View arduinoView = findViewById(R.id.imageview_main_arduino);
+        SCViewAnimation arduinoAnimation = new SCViewAnimation(arduinoView);
+        arduinoAnimation.startToPosition(size.x * 2, null);
+        arduinoAnimation.addPageAnimation(new SCPositionAnimation(this, 2, - size.x *2, 0));
+        arduinoAnimation.addPageAnimation(new SCPositionAnimation(this, 3, - size.x, 0));
+        mViewPager.addAnimation(arduinoAnimation);
+
+        // 点击 立即开始 按钮
+        View raspberryView = findViewById(R.id.imageview_main_raspberry_pi);
+        SCViewAnimation raspberryAnimation = new SCViewAnimation(raspberryView);
+        raspberryAnimation.startToPosition(-size.x, null);
+        raspberryAnimation.addPageAnimation(new SCPositionAnimation(this, 2, size.x, 0));
+        raspberryAnimation.addPageAnimation(new SCPositionAnimation(this, 3, -size.x, 0));
+        mViewPager.addAnimation(raspberryAnimation);
+        // 点击开始按钮，隐藏view Pager
+        raspberryView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View ctn = findViewById(R.id.view_pager);
+                ctn.setVisibility(View.GONE);
+            }
+        });
     }
 
 
